3e126216920e31ec31fe4aa5deaa327b83c6cd15
anhhaday <nguyenha26051988@gmail.com>
2019-10-29 17:06:33 +0700

    baitap

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,20 @@
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  login: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+  }
+  ngOnInit() {
+    this.login = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(10)]],
+    });
+  }
+}
